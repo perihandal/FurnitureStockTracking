1aa79e37a0ecccd5a4eea0e76a694a025a85e3aa
perihan dal <Perrihann60@hotmail.com>
2025-07-20 23:59:47 +0300

    ResultPatern1

---
+++ App.Services/ServiceResult.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Services
+{
+    public class ServiceResult<T>
+    {
+        public T? Data { get; set; }
+
+        public List<string>? ErrorMessage { get; set; }
+        public bool IsSuccess() =>  ErrorMessage == null || ErrorMessage.Count == 0;
+
+        public bool IsFail => !IsSuccess();
+
+        public HttpStatusCode Status { get; set; }
+
+        //static factory method
+        public static ServiceResult<T> Success(T data, HttpStatusCode status=HttpStatusCode.OK)
+        {
+            return new ServiceResult<T>()
+            {
+                Data = data,
+                Status= status
+            };
+        }
+
+        public static ServiceResult<T> Fail(List <string> errorMessage, HttpStatusCode status = HttpStatusCode.OK)
+        {
+            return new ServiceResult<T>()
+            {
+                ErrorMessage = errorMessage,
+                Status = status
+            };
+        }
+
+        public static ServiceResult<T> Fail(string errorMessage, HttpStatusCode status = HttpStatusCode.OK)
+        {
+            return new ServiceResult<T>()
+            {
+                ErrorMessage = [errorMessage],
+                Status = status
+            };
+        }
+    }
+}
